145c856662ae53c3298efd0182d627084ee3fdf0
Andrew McOlash <amcolash@gmail.com>
2015-02-12 19:34:42 -0600

    Started working on ui and simple login/logout

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,32 @@
+/* exported loggedIn */
+
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+// Sets a badge for the app - like a notification
+//chrome.browserAction.setBadgeText({text: '\'Allo'});
+
+var i = 0;
+
+var loggedIn = false;
+
+var interval;
+
+function abc() {
+  chrome.browserAction.setBadgeText({text: '' + i});
+  i = (i + 1);
+
+  if (i > 10) {
+    clearInterval(interval);
+    //loggedIn = true;
+  }
+}
+
+abc();
+
+interval = setInterval(abc, 1000);
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,30 @@
+/* global $ */
+
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+
+var background = chrome.extension.getBackgroundPage();
+
+if (!background.loggedIn) {
+  $('#login').show();
+} else {
+  $('#feed').show();
+}
+
+$('form').submit(function() {
+  event.preventDefault();
+  console.log('trying to submit!');
+  var valid = true;
+  $('input').each(function () {
+    if (!$(this).val()) {
+      valid = false;
+      console.log('invalid, ' + this);
+    }
+  });
+
+  if (valid) {
+    $('#login').hide();
+    $('#feed').show();
+  }
+});
