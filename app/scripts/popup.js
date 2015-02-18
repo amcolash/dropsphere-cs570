e2772e8860190736a54401177135741c867523b1
vash <vashmar7@gmail.com>
2015-02-18 17:54:14 -0600

    description added to drop

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,48 +1,51 @@
 /* global $ */
+$(document).ready(function(){
 
-'use strict';
+  'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+  console.log('\'Allo \'Allo! Popup');
 
-var background = chrome.extension.getBackgroundPage();
+  var background = chrome.extension.getBackgroundPage();
 
-if(!background.loggedIn){
-  $('#login').show();
-}else{
-  $('#feed').show();
-}
+  if(!background.loggedIn){
+    $('#login').show();
+  }else{
+    $('#feed').show();
+  }
 
-$('form').validator().on('submit', function (e){
-  e.preventDefault();
+  $('form').validator().on('submit', function (e){
+    e.preventDefault();
 
-  background.loggedIn = true;
+    background.loggedIn = true;
 
-  $('input').each(function() {
-    $(this).val('');
+    $('input').each(function() {
+      $(this).val('');
+    });
+
+    $('#login').hide();
+    $('#feed').show();
   });
 
-  $('#login').hide();
-  $('#feed').show();
-});
+  $('#logout').click(function(){
+    background.loggedIn = false;
+    $('#login').show();
+    $('#feed').hide();
+  });
 
-$('#logout').click(function(){
-  background.loggedIn = false;
-  $('#login').show();
-  $('#feed').hide();
-});
-
-$('#contacts li').click(function(){
-  var contact = $(this).children('.contactName').html(); 
-  $('#contacts li').removeClass('selected');
-  $('#currentContactName').html(contact);
-  $(this).addClass('selected');
-});
+  $('#contacts li').click(function(){
+    var contact = $(this).children('.contactName').html(); 
+    $('#contacts li').removeClass('selected');
+    $('#currentContactName').html(contact);
+    $(this).addClass('selected');
+  });
 
 
-$('#dropPage').click(function(){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, "drop", function(response){
-       $("#chatBox ul").append("<li class='messageRight'><a target='_blank' href= '" + response.link + " '>" + response.title + "</a></li>");
+  $('#dropPage').click(function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      chrome.tabs.sendMessage(tabs[0].id, "drop", function(response){
+         $("#chatBox ul").append("<li class='messageRight'><a target='_blank' href= '" + response.link + " '>" + response.title + "</a> <p>" + response.description + " </p></li>");  
+      });
     });
   });
+
 });
