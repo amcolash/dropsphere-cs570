59ef974f036ba4b80494c2712abda13dffe90961
Andrew McOlash <amcolash@gmail.com>
2015-02-24 15:53:25 -0600

    Added some fun :)

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -84,26 +84,26 @@ $(function() {
   });
 
   ////////////////////////////////////// drag + drop share events ///////////////////////////////////////////////////////////////
-  
-  var draggedContent; 
-  var convoSwitched = false; 
+
+  var draggedContent;
+  var convoSwitched = false;
 
   $("#chatBox").on("dragstart", ".linkItem",  function(event){
     draggedContent = this.outerHTML;
   });
 
   $('#contacts li').on("dragover", function(event){
-    event.preventDefault();  
+    event.preventDefault();
     $(this).addClass('selected');
   });
 
   $('#contacts li').on("dragleave", function(event){
-    event.preventDefault();  
+    event.preventDefault();
     $(this).removeClass('selected');
   });
 
   $('#contacts li').on("drop", function(event){
-    event.preventDefault();  
+    event.preventDefault();
     var contact = $(this).children('.contactName').html();
     if(contact != background.currentConvo){
       background.switchConvo(contact);
@@ -111,7 +111,7 @@ $(function() {
       fillConvo();
       $('#contacts li').removeClass('selected');
       $(this).addClass('selected');
-      convoSwitched = false; 
+      convoSwitched = false;
       setTimeout(fakeResponse, 1000);
     }
   });
@@ -122,7 +122,7 @@ $(function() {
     var contact = $(this).children('.contactName').html();
     $('#contacts li').removeClass('selected');
     $(this).addClass('selected');
-    convoSwitched = true; 
+    convoSwitched = true;
     fillConvo(contact);
   });
 
@@ -157,7 +157,7 @@ $(function() {
               var msgElement  = '<li class="messageRight">' + msg + '</li> <br>';
               $('#chatBox').append(msgElement);
               $('#dropContent').val("");
-              background.storeMessage(msgElement);   
+              background.storeMessage(msgElement);
               var scrollTo = $('#chatBox').prop('scrollHeight') + 'px';
               $('#chatBox').slimScroll({ scrollTo : scrollTo });
 
@@ -178,7 +178,7 @@ $(function() {
   }
 
   function filterLinks(hide){
-    convoSwitched = true; 
+    convoSwitched = true;
     $('#chatBox li').each(function(){
       if(!($(this).hasClass('linkItem'))){
         if (hide) {
@@ -193,18 +193,18 @@ $(function() {
       }
     });
   }
-
+ Untitled document
 
   function fakeResponse(){
     if(!convoSwitched && background.currentConvo != "Me"){
-      var responses = ['Awesome!', 'Thats so cool!', 'meh', 'I want more!', 'Seen it already'];
+      var responses = ['Awesome!', 'Thats so cool!', 'meh', 'I want more!', 'Seen it already', 'That is so fetch!'];
       var msg  = responses[Math.floor(Math.random() * (responses.length - 1)) + 0];
       var msgElement  = '<li class="messageLeft">' + msg + '</li> <br>';
       $('#chatBox').append(msgElement);
       var scrollTo = $('#chatBox').prop('scrollHeight') + 'px';
       $('#chatBox').slimScroll({ scrollTo : scrollTo });
-          
-      background.storeMessage(msgElement); 
+
+      background.storeMessage(msgElement);
     }
   }
 
